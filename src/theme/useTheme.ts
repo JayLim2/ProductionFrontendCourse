72c7d17922034b421a219f60cab5827ded829199
JayLim <ddx180874@gmail.com>
2023-02-11 00:57:05 +0400

    added several themes; implemented mechanism for toggling & storing themes; implemented in React-hook style

---
+++ src/theme/useTheme.ts
@@ -0,0 +1,19 @@
+import {Theme, THEME_PROPERTY, ThemeContext} from "./ThemeContext";
+import {useContext} from "react";
+
+export interface UseTheme {
+    toggleTheme: () => void,
+    theme: Theme
+}
+
+export function useTheme(): UseTheme {
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.DEFAULT ? Theme.DARK : Theme.DEFAULT;
+        localStorage.setItem(THEME_PROPERTY, newTheme);
+        setTheme(newTheme);
+    }
+
+    return {theme, toggleTheme};
+}
