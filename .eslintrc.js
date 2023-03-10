b81ba623d9754bbb36ec36f2fb4b4d4ba0931727
JayLim <ddx180874@gmail.com>
2023-03-11 00:10:35 +0400

    fixed eslint errors with literal strings in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,48 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:i18next/recommended'
-  ],
-  overrides: [
-      {
-          files: ["*.ts", "*.tsx"]
-      }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json']
-  },
-  plugins: [
-    'react', 'i18next'
-  ],
-  rules: {
-    '@typescript-eslint/semi': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'i18next/no-literal-string': ['error', {markupOnly: true, ignoreAttribute: ["to", "className", "data-testid"]}],
-    '@typescript-eslint/space-before-function-paren': 'off'
-  }
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true
+    },
+    extends: [
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:i18next/recommended'
+    ],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"]
+        },
+        {
+            files: ["*.spec.ts", "*.spec.tsx"],
+            rules: {
+                'i18next/no-literal-string': 'off'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json']
+    },
+    plugins: [
+        'react', 'i18next'
+    ],
+    rules: {
+        '@typescript-eslint/semi': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: [
+                    "to", "className", "data-testid"
+                ]
+            }
+        ],
+        '@typescript-eslint/space-before-function-paren': 'off'
+    }
 }
