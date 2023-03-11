fc4bfb1439108d5a28852a5c7b28b46d16d10e49
JayLim <ddx180874@gmail.com>
2023-03-11 20:22:02 +0400

    fixes according to linters recommendations

--- src/widgets/NavigationBar/ui/NavigationBar.tsx
+++ src/widgets/NavigationBar/ui/NavigationBar.tsx
@@ -1,7 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import styles from './NavigationBar.module.scss'
-import { UxLink, UxLinkTheme } from 'shared/ui/UxLink/UxLink'
-import { useTranslation } from 'react-i18next'
 import { type FC } from 'react'
 
 interface NavigationBarProps {
@@ -10,12 +8,10 @@ interface NavigationBarProps {
 
 export const NavigationBar: FC<NavigationBarProps> = (props) => {
   const { className } = props
-  const { t } = useTranslation()
 
   return (
         <div className={classNames(styles.NavigationBar, {}, [className])}>
             <div className={styles.links}>
-
             </div>
         </div>
   )
