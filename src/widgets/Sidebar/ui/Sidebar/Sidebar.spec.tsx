a7ab331f8781f98c567536c2f9ec32d3938835ae
JayLim <ddx180874@gmail.com>
2023-03-11 20:00:33 +0400

    added HOC 'renderComponent' for routing pages & translating text in tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.spec.tsx
@@ -1,16 +1,16 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
-import renderWithTranslation from 'shared/lib/renderWithTranslation/renderWithTranslation';
+import { renderComponent } from 'shared/lib/tests/renderComponent/renderComponent';
 
 describe('Sidebar', () => {
   test('Sidebar is present', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     const sidebar = screen.getByTestId('sidebar');
     expect(sidebar).toBeInTheDocument();
   });
 
   test('Sidebar is collapsable', () => {
-    renderWithTranslation(<Sidebar />);
+    renderComponent(<Sidebar />);
     const button = screen.getByTestId('sidebar-toggle');
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
